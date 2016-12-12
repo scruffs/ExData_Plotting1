926dc283ffdaa823f5c02c3f1456774c699fa207
scruffs <davenenglish@gmail.com>
2016-12-12 17:39:28 +0000

    Produced plot1 script and png file.

---
+++ plot1.R
@@ -0,0 +1,10 @@
+plot1 <- function() {
+      
+      png(filename = "plot1.png",
+          width = 480, height = 480)
+      hist(energy$Global_active_power, 
+           xlab = "Global Active Power (kilowatts)", 
+           col = "red", 
+           main = "Global Active Power")
+      dev.off()
+}
